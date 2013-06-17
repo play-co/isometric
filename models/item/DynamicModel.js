a88b40f4db9ae05f028d23a989bef6cf260e91bf
Arno van der Vegt <arno@gameclosure.com>
2013-06-16 19:27:33 -0700

    Fixed z-index

--- models/item/DynamicModel.js
+++ models/item/DynamicModel.js
@@ -394,7 +394,7 @@ exports = Class(Emitter, function (supr) {
 
 			this._move(dt);
 			opts.dt = dt;
-			opts.zIndex = this._zIndex + ((opts.y * 4) | 0) + ((opts.x * 2) | 0);
+			opts.zIndex = this._zIndex + ((opts.y * 4) | 0) + ((2 - opts.x * 2) | 0);
 
 			this._updateCB && this._updateCB(opts);
 		}
